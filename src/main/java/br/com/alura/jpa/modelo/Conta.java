2b459ca24fdb68bd8b8a7e69b0c2bc291962a2fe
leonardocordeiro <leonardo.cordeiro@m4u.com.br>
2020-07-28 01:10:55 -0300

    projeto inicial

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,13 +1,9 @@
 package br.com.alura.jpa.modelo;
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -18,12 +14,13 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private Double saldo;
-	@OneToMany(mappedBy="conta", fetch = FetchType.EAGER)
-	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
+	
+	
+
 
 	public Long getId() {
 		return id;
@@ -55,9 +52,5 @@ public class Conta {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
-	}
-
-	public List<Movimentacao> getMovimentacoes() {
-		return movimentacoes;
 	}
 }
