e2eccf9a792bbab02f9ec1eedb83025b58a2cd81
leonardocordeiro <leonardo.cordeiro@m4u.com.br>
2020-07-28 00:47:13 -0300

    aula 4

--- src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
+++ src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
@@ -21,39 +21,6 @@ public class MovimentacaoDao {
 		this.em = em;
 	}
 	
-	public List<Movimentacao> getMovimentacoesFiltradasPorData(Integer dia, Integer mes, Integer ano) { 
-		
-		CriteriaBuilder builder = em.getCriteriaBuilder();
-		CriteriaQuery<Movimentacao> query = builder.createQuery(Movimentacao.class);
-		
-		Root<Movimentacao> root = query.from(Movimentacao.class);
-		
-		List<Predicate> predicates = new ArrayList<>();
-		
-		if(dia != null) { 
-			// day(m.data)
-			Predicate predicate = builder.equal(builder.function("day", Integer.class, root.get("data")), dia);
-			predicates.add(predicate);
-		}
-		
-		if(mes != null) { 
-			// month(m.data)
-			Predicate predicate = builder.equal(builder.function("month", Integer.class, root.get("data")), mes);
-			predicates.add(predicate);
-		}
-		
-		if(ano != null) { 
-			// year(m.data)
-			Predicate predicate = builder.equal(builder.function("year", Integer.class, root.get("data")), ano);
-			predicates.add(predicate);
-		}
-		
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-		
-		TypedQuery<Movimentacao> typedQuery = em.createQuery(query);
-		return typedQuery.getResultList();
-	}
-
 	public List<MediaComData> getMediaDiariaDasMovimentacoes() {
 		String jpql = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
 
